582d549c49d6745f448020fdf0ac47819db78dbb
MegaChie <workspace.mega@gmail.com>
2024-06-16 10:42:19 -0700

    Make it into a user-defined-function

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,25 +3,27 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client.logs
-colct = db.nginx
-logsCount = colct.count_documents({})
-getCount = colct.count_documents({"method": "GET"})
-postCount = colct.count_documents({"method": "POST"})
-putCount = colct.count_documents({"method": "PUT"})
-patchCount = colct.count_documents({"method": "PATCH"})
-deletCount = colct.count_documents({"method": "DELETE"})
-checkCount = colct.count_documents({"method": "GET", "path": "/status"})
-stats = """{} logs
-Methods:
-    method GET: {}
-    method POST: {}
-    method PUT: {}
-    method PATCH: {}
-    method DELETE: {}
-{} status check""".format(logsCount, getCount,
-                          postCount, putCount,
-                          patchCount, deletCount,
-                          checkCount)
-print(stats)
+def statsView():
+    """Prints statistics about the database"""
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.logs
+    colct = db.nginx
+    logsCount = colct.count_documents({})
+    getCount = colct.count_documents({"method": "GET"})
+    postCount = colct.count_documents({"method": "POST"})
+    putCount = colct.count_documents({"method": "PUT"})
+    patchCount = colct.count_documents({"method": "PATCH"})
+    deletCount = colct.count_documents({"method": "DELETE"})
+    checkCount = colct.count_documents({"method": "GET", "path": "/status"})
+    print("{} logs".format(logsCount))
+    print("Methods:")
+    print("\tmethod GET: {}".format(getCount))
+    print("\tmethod POST: {}".format(postCount))
+    print("\tmethod PUT: {}".format(putCount))
+    print("\tmethod PATCH: {}".format(patchCount))
+    print("\tmethod DELETE: {}".format(deletCount))
+    print("{} status check".format(checkCount))
+
+
+if __name__ == "__main__":
+    statsView()
