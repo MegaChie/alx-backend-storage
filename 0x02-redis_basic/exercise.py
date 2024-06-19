928758e29b9cd8ed27a0724e219ab29df4f220e1
MegaChie <workspace.mega@gmail.com>
2024-06-19 12:36:26 +0000

    Reads from Redis and converts to original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        """Convert the data back to the desired format and returns it"""
+        res = self._redis.get(key)
+        if res and fn:
+            res = fn(res)
+        return res
+
+    def get_str(self, key: str) -> str:
+        """Convert into string"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Convert into integer"""
+        return self.get(key, int)
